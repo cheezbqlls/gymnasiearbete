c472bcf5d214b759ca2ed39a1522ab7982638cf5
Hannah :3 <123725147+cheezbqlls@users.noreply.github.com>
2025-02-05 12:55:22 +0100

    mrew

--- GymnasieArbete/Assets/Scripts/Hannah/TimingBarController.cs
+++ GymnasieArbete/Assets/Scripts/Hannah/TimingBarController.cs
@@ -3,34 +3,47 @@ using UnityEngine;
 public class TimingBarController : MonoBehaviour
 {
     public Transform Indicator;
-    public Transform greenZone; 
+    public Transform greenZone;
 
-    public float moveSpeed = 0.05f;
+    public float moveSpeed = 0.01f;
     private bool movingRight = true;
+    private bool isStopped = false;
 
     void Update()
     {
-        MoveIndicator();
-
-        if (Input.GetMouseButtonDown(0)) 
+        if (!isStopped)
         {
-            CheckSuccess();
+            MoveIndicator();
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                isStopped = true;
+                CheckSuccess();
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ResetIndicator();
+            }
         }
     }
 
     void MoveIndicator()
     {
         float moveAmount = moveSpeed * Time.deltaTime;
+
         if (movingRight)
         {
             Indicator.localPosition += new Vector3(moveAmount, 0, 0);
-            if (Indicator.localPosition.x >= 0.5)
+            if (Indicator.localPosition.x >= 0.5f)
                 movingRight = false;
         }
         else
         {
             Indicator.localPosition -= new Vector3(moveAmount, 0, 0);
-            if (Indicator.localPosition.x <= -0.5)
+            if (Indicator.localPosition.x <= -0.5f)
                 movingRight = true;
         }
     }
@@ -38,6 +51,19 @@ public class TimingBarController : MonoBehaviour
     void CheckSuccess()
     {
         float distance = Mathf.Abs(Indicator.position.x - greenZone.position.x);
-        if (distance < 0.5f);
+
+        if (distance < 0.5f)
+        {
+        }
+        else
+        {
+        }
+    }
+
+    void ResetIndicator()
+    {
+        Indicator.localPosition = new Vector3(-2.5f, Indicator.localPosition.y, Indicator.localPosition.z);
+        isStopped = false;
+        movingRight = true;
     }
 }
