732ea42d641128a804bb0abf823f1541f2e312d3
hanapusfukr <Hana.Nikkelen@elev.ga.lbs.se>
2025-03-31 15:23:07 +0200

    ÄNTLIGEN

--- GymnasieArbete/Assets/Scripts/Hana/Clicked.cs
+++ GymnasieArbete/Assets/Scripts/Hana/Clicked.cs
@@ -6,17 +6,27 @@ public class Clicked : MonoBehaviour
 {
     public int count;
     public string clicked;
-    public void OnMouseDown()
+    public GameObject light;
+    public Kontrol kontrol;
+
+    public async void OnMouseDown()
     {
         Debug.Log("yes");
         count += 1;
-        
-        
+        light.SetActive(true);
+        Invoke("TurnOffLight", 1f);
+        kontrol.OnColorClicked(gameObject.name);
+
+    }
+
+    public void TurnOffLight()
+    {
+        light.SetActive(false);
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        light.SetActive(false);
     }
 
     // Update is called once per frame
--- GymnasieArbete/Assets/Scripts/Hana/Kontrol.cs
+++ GymnasieArbete/Assets/Scripts/Hana/Kontrol.cs
@@ -4,131 +4,83 @@ using UnityEngine;
 
 public class Kontrol : MonoBehaviour
 {
-    public static List<string> colors = new List<string>();
-    public static List<string> order = new List<string>();
-    public static List<string> check = new List<string>();
-    int choise;
-    public Clicked Redscript;
-    public Clicked Bluescript;
-    public Clicked Greenscript;
-    public Clicked Yellowscript;
-    public int totalcount;
+    public string[] colors = { "Red", "Green", "Yellow", "Blue" };
+    public List<string> order = new List<string>();
+    public List<string> check = new List<string>();
 
-    // Start is called before the first frame update
+
+    public int step = 0;
+    public bool playerTurn = false;
     void Start()
     {
+        GenerateSequence(4); 
         
     }
+
+    // Generate a random sequence
+    void GenerateSequence(int length)
+    {
+        order.Clear();
+        for (int i = 0; i < length; i++)
+        {
+            order.Add(colors[Random.Range(0, colors.Length)]);
+        }
+    }
+    System.Collections.IEnumerator PlaySequence()
+    {
+        playerTurn = false;
+        yield return new WaitForSeconds(1f);
+
+        foreach (string color in order)
+        {
+            Debug.Log("Simon Says: " + color);
+            yield return new WaitForSeconds(1f);
+        }
+
+        playerTurn = true;
+        step = 0;
+        check.Clear();
+    }
+
+    // Call this when a color button is clicked
+    public void OnColorClicked(string color)
+    {
+        if (!playerTurn) return;
+
+        check.Add(color);
+        Debug.Log("Player clicked: " + color);
+
+        if (check[step] != order[step])
+        {
+            Debug.Log("Wrong Color! Game Over.");
+            ResetGame();
+            return;
+        }
+
+        step++;
+
+        if (step == order.Count)
+        {
+            Debug.Log("Correct Sequence! Next Round.");
+            StartCoroutine(PlaySequence());
+        }
+    }
+
+    void ResetGame()
+    {
+        step = 0;
+        check.Clear();
+        GenerateSequence(4);
+        StartCoroutine(PlaySequence());
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        colors.Add("Red");
-        colors.Add("Blue");
-        colors.Add("Green");
-        colors.Add("Yellow");
+       
 
-        for(int i = 0; i < 5; i++)
-        {
-            choise = Random.Range(0, 4);
-            order.Add(colors[choise]);
-            Debug.Log(order[i]);
-        }
-        totalcount = Redscript.count + Bluescript.count + Greenscript.count + Yellowscript.count;
-        string counts = totalcount.ToString();
-        Debug.Log(counts);
-
-        if(Redscript.count == 1)
-        {
-            check.Add("Red");
-        }
-        if(Redscript.count == 2)
-        {
-            check.Add("Red");
-        }
-        if (Redscript.count == 3)
-        {
-            check.Add("Red");
-        }
-        if (Redscript.count == 4)
-        {
-            check.Add("Red");
-        }
-        if (Redscript.count == 5)
-        {
-            check.Add("Red");
-        }
-        if (Bluescript.count == 1)
-        {
-            check.Add("Blue");
-        }
-        if (Bluescript.count == 2)
-        {
-            check.Add("Blue");
-        }
-        if (Bluescript.count == 3)
-        {
-            check.Add("Blue");
-        }
-        if (Bluescript.count == 4)
-        {
-            check.Add("Blue");
-        }
-        if (Bluescript.count == 5)
-        {
-            check.Add("Blue");
-        }
-        if (Greenscript.count == 1)
-        {
-            check.Add("Green");
-        }
-        if (Greenscript.count == 2)
-        {
-            check.Add("Green");
-        }
-        if (Greenscript.count == 3)
-        {
-            check.Add("Green");
-        }
-        if (Greenscript.count == 4)
-        {
-            check.Add("Green");
-        }
-        if (Greenscript.count == 5)
-        {
-            check.Add("Green");
-        }
-        if (Yellowscript.count == 1)
-        {
-            check.Add("Yellow");
-        }
-        if (Yellowscript.count == 2)
-        {
-            check.Add("Yellow");
-        }
-        if (Yellowscript.count == 3)
-        {
-            check.Add("Yellow");
-        }
-        if (Yellowscript.count == 4)
-        {
-            check.Add("Yellow");
-        }
-        if (Yellowscript.count == 5)
-        {
-            check.Add("Yellow");
-        }
-        for(int n = 0; n<5; n++)
-        {
-            if (order[n] == check[n])
-            {
-                Debug.Log("Correct");
-            }
-            else
-            {
-                Debug.Log("False");
-            }
-        }
+        
     }
 
 
